cc161c48a6f3639f96b27a7ef517f114e93ac1fa
mbax4ab4 <anthony.bryan@student.manchester.ac.uk>
2014-11-27 21:45:58 +0000

    certainly getting there

--- laravel/public/contacts.php
+++ laravel/public/contacts.php
@@ -29,42 +29,85 @@
   }
   
   $cookie_name = "id";
-  $id = $_COOKIE[$cookie_name];
-  // echo "your id = ". $id;
+
+  $id= $_COOKIE[$cookie_name];
+
   echo "<hr>";
 
-  // ----------------------------------------------------------------------
-  // Get all the users who this user added and output them
-  $query = "SELECT * FROM friends WHERE user1ID = '$id'";
-  $result = mysqli_query($mysqli, $query) or die($mysqli_error());
-   
-  while($row = $result->fetch_array())
-  {
-    $rows[] = $row;
-  }
   
-  foreach($rows as $row)
-  {
-    $user2ID = $row['user2ID'];
-    $query = "SELECT * FROM users WHERE id = '$user2ID'";
-    $result = $mysqli->query($query);
-    $row2 = $result->fetch_assoc();
-    // friend id is  $user2ID;
-    if ($user2ID != $id)
-    {
-      echo "Name  : " . $row2['first_name'] . $row2['last_name'] . "<br>";
-      echo "Email : " . $row2['email'] . "<br>";
-      echo "<hr>";
+    // ----------------------------------------------------------------------
+    // Get all the users who this user added and output them
+    $query = "SELECT * FROM friends WHERE user1ID = '$id'";
+    $result = mysqli_query($mysqli, $query) or die($mysqli_error());
     
+    $num_row = mysqli_num_rows($result);
+  
+  if($num_row !=0)
+  {
+    while($row = $result->fetch_array())
+    {
+      $rows[] = $row;
     }
-  }
 
+    foreach($rows as $row)
+    {
+      $user2ID = $row['user2ID'];
+      $query = "SELECT * FROM users WHERE id = '$user2ID'";
+      $result = $mysqli->query($query);
+      $row2 = $result->fetch_assoc();
+      // friend id is  $user2ID;
+      if ($user2ID != $id)
+      {
+	echo "Name  : " . $row2['first_name'] . $row2['last_name'] . "<br>";
+	echo "Email : " . $row2['email'] . "<br>";
+	echo "<hr>";
+
+      }
+    }
+  } // if results
+  else
+  {
+    echo "You have no friends yet :( <br>";
+    echo "Add some friends below <br>";
+  }
   $result->close();
 ?>
 
 <h2> Add a friend by email </h2>
 
 
+
+
+<form name="form1" method="post" action="contacts.php">
+	<input name="search" type="text" /> 
+	<input type="submit" name="submit" value="search" /> 
+
+
+</form>
+
+<?php
+if(isset($_POST['search']))
+{
+if (strlen (($_POST['search'])) > 5)
+{
+  $search = $_POST['search'];
+  $query = "SELECT * FROM users WHERE email = '$search'";
+  $result = mysqli_query($mysqli, $query) or die($mysqli_error());
+  $num_row = mysqli_num_rows($result);
+  if($num_row == 0)
+ { echo "No results found"; }
+   else
+   { 
+   $row = $result -> fetch_assoc();
+
+   echo "Name  : " . $row['first_name'] . $row['last_name'] . "<br>";
+   echo "Email : " . $row['email'] . "<br>";
+    
+  }  
+}
+}
+
+?>
 
 
 
