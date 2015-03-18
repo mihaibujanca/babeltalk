de104cbcad1a111d4ed969e9adadcdd06d992344
Anthony <anthony.bryan@student.manchester.ac.uk>
2015-03-18 23:25:47 +0000

    chatter

---
+++ laravel/public/chat.php
@@ -0,0 +1,39 @@
+<html>
+<head> </head>
+  <body>
+  <?php
+    // load configuration for database
+    require_once('config.inc.php'); 
+    // connect to database
+    $mysqli = new mysqli($database_host, $database_user, $database_password, $database_name);
+    // check for error 
+    if($mysqli -> connect_error) 
+    {
+      die('Connection Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+    }
+
+    // test with a conversation between user 24 and 25
+    // play as user 24
+    $id = 24;
+    $partnerid = 25;
+    
+    $query = "SELECT * FROM exchanges WHERE (senderID = '$id' AND receiverID = '$partnerid') OR (senderID = '$partnerid' AND receiverID = '$id') ORDER BY id";
+    $result = mysqli_query($mysqli, $query) or die($mysqli_error());
+    $num_row = mysqli_num_rows($result);
+    if($num_row == 0)
+    { 
+      echo "<p> No messages </p>"; 
+    }
+    else
+    {
+      while ($row = mysqli_fetch_row($result))
+      {
+	echo $row[5] . "<br>"; 
+      }
+    }
+    
+    $result->close();
+  ?>
+  </body>
+</html> 
+	  
