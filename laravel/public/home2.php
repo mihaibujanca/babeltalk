ddad36a653919c94b9989f7031b201287ba23371
Anthony <anthony.bryan@student.manchester.ac.uk>
2015-03-18 20:29:16 +0000

    tidy up

--- laravel/public/home2.php
+++ laravel/public/home2.php
@@ -17,7 +17,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Babel talk| home2</title>
+    <title>Babel talk | Home</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
