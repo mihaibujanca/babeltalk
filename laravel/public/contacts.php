7f87c4e8d68637cbafed8e521f0127ce0c89f478
Anthony <anthony.bryan@student.manchester.ac.uk>
2015-03-18 10:31:16 +0000

    hmm

--- laravel/public/contacts.php
+++ laravel/public/contacts.php
@@ -105,7 +105,66 @@
 
 	</form>
 
-	<?php
+<?php
+      if (isset($_POST['id']))
+      {
+	  // code to load and output friends list
+	  
+	  // load configuration for database
+	  require_once('config.inc.php'); 
+	  // connect to database
+	  $mysqli = new mysqli($database_host, $database_user, $database_password, $database_name);
+	  // check for error 
+	  if($mysqli -> connect_error) 
+	  {
+	    die('Connection Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+	  }
+	  
+	  $cookie_name = "id";
+
+	  $id= $_COOKIE[$cookie_name];
+
+	  echo "<hr>";
+
+	  
+	    // ----------------------------------------------------------------------
+	    // Get all the users who this user added and output them
+	    $query = "SELECT * FROM friends WHERE user1ID = '$id'";
+	    $result = mysqli_query($mysqli, $query) or die($mysqli_error());
+	    
+	    $num_row = mysqli_num_rows($result);
+	  
+	  if($num_row !=0)
+	  {
+	    while($row = $result->fetch_array())
+	    {
+	      $rows[] = $row;
+	    }
+
+	    foreach($rows as $row)
+	    {
+	      $user2ID = $row['user2ID'];
+	      $query = "SELECT * FROM users WHERE id = '$user2ID'";
+	      $result = $mysqli->query($query);
+	      $row2 = $result->fetch_assoc();
+	      // friend id is  $user2ID;
+	      if ($user2ID != $id)
+	      {
+		echo "Name  : " . $row2['first_name'] . $row2['last_name'] . "<br>";
+		echo "Email : " . $row2['email'] . "<br>";
+		echo "<hr>";
+
+	      }
+	    }
+	  } // if results
+	  else
+	  {
+	    echo "You have no friends yet :( <br>";
+	    echo "Add some friends below <br>";
+	  }
+	  $result->close();
+
+
 	if(isset($_POST['search']))
 	{
 	if (strlen (($_POST['search'])) > 5)
