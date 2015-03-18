76ad1744bd3e1351d363b7a88c2fb5e850a72755
Anthony <anthony.bryan@student.manchester.ac.uk>
2015-03-18 15:11:51 +0000

    pretty

--- laravel/public/contacts.php
+++ laravel/public/contacts.php
@@ -230,9 +230,9 @@
 	      // friend id is  $user2ID;
 	      if ($user2ID != $id)
 	      {	
-		echo "<span style='display: inline-block; padding: 10px;'>";  
+		echo "<span style='display: inline-block;'>";  
 		echo "<form name='chat' method='post' action='contacts.php' style='display: inline-block; padding: 10px;'>"; 
-		echo "<button type='submit' name='chat' value='".$row2['id']."'> <image style='width:14px;height:14px;padding:10px;' src='http://www.famfamfam.com/lab/icons/mini/icons/comment.gif'/> </button>"; ; 
+		echo "<button type='submit' name='chat' value='".$row2['id']."' style=padding:10px;> <image style='width:14px;height:14px;' src='http://www.famfamfam.com/lab/icons/mini/icons/comment.gif'/> </button>"; ; 
 		echo "</form>";
 		printFlag($row2['language']);
 		echo "<b> " . $row2['first_name'] . " " . $row2['last_name'] . "</b> ";
