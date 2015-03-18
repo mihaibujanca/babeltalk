55a658e0d680e8cbe15f06eed8d0a2410126f11a
Anthony <anthony.bryan@student.manchester.ac.uk>
2015-03-18 12:22:13 +0000

    add a friend

--- laravel/public/contacts.php
+++ laravel/public/contacts.php
@@ -177,6 +177,8 @@
 	  echo "adding a friend";
 	  INSERT INTO friends (user1ID, user2ID)
 	  VALUES ($id , $_POST['add']);
+	  $query = "INSERT INTO friends (user1ID, user2ID) VALUES ($id , $_POST['add'])";
+	  $result = mysqli_query($mysqli, $query) or die($mysqli_error());
 	
 	} // is set add
 
