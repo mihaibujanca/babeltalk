6040c9da56eae8dcaaca5785c52edc24ce9d0d36
Anthony <anthony.bryan@student.manchester.ac.uk>
2015-03-18 20:23:48 +0000

    redirect user to index.html on logout

--- laravel/public/signout.php
+++ laravel/public/signout.php
@@ -3,5 +3,5 @@
  $cookie_value = "";
  // set the expiration date to one hour ago
 setcookie($cookie_name, $cookie_value, time() - 3600, "/");
-header("Location: http://10.2.234.107/babeltalk/laravel/public/home2.html");
+header("Location: http://10.2.234.107/babeltalk/laravel/public/index.html");
 ?>
