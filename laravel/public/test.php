b6bbf4ff5a12fd51d4b8ba248549e9800fea44e6
Ryan Carruthers <ryan-carruthers-2@student.manchester.ac.uk>
2015-03-19 19:49:38 +0000

    changes

--- laravel/public/test.php
+++ laravel/public/test.php
@@ -14,7 +14,7 @@
 <?php
 
   
-  $message = $_POST('message');
+  $message = $_POST["message"];
   $newstring = implode("+", preg_split("/[\s]+/", $message));
 
   $contents = file_get_contents("http://itranslate4.eu/api/Translate?auth=df287e5e-6b1f-4319-90c7-9bc3ba3e45c4&src=de&trg=en&dat=Andreas hat seine Aufgabe erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
