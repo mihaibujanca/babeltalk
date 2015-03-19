5e519b72767fd1cdeceed417f785d2b9ba534da5
Ryan Carruthers <ryan-carruthers-2@student.manchester.ac.uk>
2015-03-19 18:33:39 +0000

    changes

--- laravel/public/test.php
+++ laravel/public/test.php
@@ -2,9 +2,8 @@
 
   $translation = file_get_contents("http://itranslate4.eu/api/Translate?auth=df287e5e-6b1f-4319-90c7-9bc3ba3e45c4&src=de&trg=en&dat=Andreas+hat+seine+Aufgabe+erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
 
-  $array = json_decode($json, true);
+  echo $translation;
    
-  echo $array;
 
 
 ?>
