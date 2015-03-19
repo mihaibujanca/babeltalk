aad29428cae02fc972e2c6c6684a41ed4715c828
Ryan Carruthers <ryan-carruthers-2@student.manchester.ac.uk>
2015-03-19 19:51:46 +0000

    changes

--- laravel/public/test.php
+++ laravel/public/test.php
@@ -11,15 +11,15 @@
   Enter a message:  <input type="text" name="message"><br>
   <input type="submit">
   </form>
-<?php
+  <?php
 
-  
   $message = $_POST["message"];
+
   $newstring = implode("+", preg_split("/[\s]+/", $message));
 
   $contents = file_get_contents("http://itranslate4.eu/api/Translate?auth=df287e5e-6b1f-4319-90c7-9bc3ba3e45c4&src=de&trg=en&dat=Andreas hat seine Aufgabe erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
 
-  echo $
+  echo $newstring;
  
   $contents = file_get_contents("http://itranslate4.eu/api/Translate?auth=df287e5e-6b1f-4319-90c7-9bc3ba3e45c4&src=de&trg=en&dat=Andreas+hat+seine+Aufgabe+erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
 
