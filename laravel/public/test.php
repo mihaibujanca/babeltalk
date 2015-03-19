05c98bc026b5078aaed4ba6ed2b37ab47577e213
Ryan Carruthers <ryan-carruthers-2@student.manchester.ac.uk>
2015-03-19 18:43:29 +0000

    changes

--- laravel/public/test.php
+++ laravel/public/test.php
@@ -1,10 +1,15 @@
 <?php
 
-  $json = file_get_contents("http://itranslate4.eu/api/Translate?auth=df287e5e-6b1f-4319-90c7-9bc3ba3e45c4&src=de&trg=en&dat=Andreas+hat+seine+Aufgabe+erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
+  $contents = file_get_contents("http://itranslate4.eu/api/Translate?auth=df287e5e-6b1f-4319-90c7-9bc3ba3e45c4&src=de&trg=en&dat=Andreas+hat+seine+Aufgabe+erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
 
 
-  $array = json_decode($json, true);
+  $json = json_decode($contents, true);
    
-  echo $array['text'];
+  foreach($json['dat'] as $item) {
+  
+    echo $item; 
+
+  }
+
 
 ?>
