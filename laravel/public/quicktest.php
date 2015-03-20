1ad6a8c723ab967e0ccef54481722915e77c4b7b
ryancarruthers <cruddyr@live.co.uk>
2015-03-20 05:39:24 +0000

    quick

---
+++ laravel/public/quicktest.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+            
+    </head>
+    
+    <body>
+        <?php
+            echo file_get_contents("http://itranslate4.eu/api/Translate?auth=785f2f42-eab1-461d-8a72-a1867112458a&src=de&trg=en&dat=Andreas+hat+seine+Aufgabe+erledigt.&dat=Er+kann+jetzt+nach+Hause+gehen.");
+        ?>
+    </body>
+</html>
