87a911b919542fad9421ae12ad65a4244244036e
tonytony99 <anthony.bryan@outlook.com>
2015-03-20 07:44:01 +0000

    a

--- laravel/public/send.php
+++ laravel/public/send.php
@@ -1,7 +1,23 @@
 <?php
-  if (isset($_POST["content"]))
-  {
-      echo '<hr>'; 
-  }
+  $content = $_POST["content"];
+
+  // pickup cookies
+  $id= $_COOKIE["id"];
+  $firstname= $_COOKIE["firstname"];
+  $lastname= $_COOKIE["lastname"];
+  if (isset($_COOKIE["language"])){
+    setcookie("language","en");
+  } // if language cookie
+  $language = $_COOKIE["language"];
+  
+  $partnerID= $_COOKIE["partnerID"];
+  $partnerfirstname= $_COOKIE["partnerfirstname"];
+  $partnerlastname = $_COOKIE["partnerlastname"];
+  $partnerlanguage = $_COOKIE["partnerlanguage"];
+  $time = time();
+  
+  $insert_row = $mysqli->query("INSERT INTO exchanges (senderID, receiverID, time, content) 
+				    VALUES  ('$id', '$partnerID', '$time', '$content', '$content')");
+      
   header("Location: https://web.cs.manchester.ac.uk/mbax4ab4/babeltalk/laravel/public/testinput.php");
 ?>
