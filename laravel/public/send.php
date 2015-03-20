943724a123353c4c1c34e8b09cc2cbd3ebf9e8cf
ryancarruthers <cruddyr@live.co.uk>
2015-03-20 09:46:13 +0000

    alert

--- laravel/public/send.php
+++ laravel/public/send.php
@@ -54,7 +54,7 @@
      
      // Call the API
      $url = "http://itranslate4.eu/api/Translate?auth=785f2f42-eab1-461d-8a72-a1867112458a&src=" . $senderlang . "&trg=" . $recieverlang . "&dat=" . $message . ".";
-     
+     echo $url;
      // Get the contents of the URL
      $contents = url_get_contents($url);
 
